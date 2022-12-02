8eb15117c0a5361acff09fef20fb48d3ee77b95d
Gesa Rizky Nugraha <109656432+gesarizky@users.noreply.github.com>
2022-12-03 05:31:32 +0700

    Add files via upload

---
+++ kode.55.tinilai.py
@@ -0,0 +1,18 @@
+# Author  : Gesa Rizky Nugraha
+# Email   : gesarizkynugraha@gmail.com
+# Website : karenabelajar.blogspot.com
+
+# Menginput
+y1 = int(input("Masukan Nilai Y1 : "))
+y2 = int(input("Masukan Nilai Y2: "))
+x1 = int(input("Masukan Nilai X1: "))
+
+# Mengkonversi
+senilai = x1 * y2 / y1
+tinilai = x1 * y1 / y2
+
+# Menampilkan Hasil
+print('==========================================================')
+print('Maka Hasil Senilai : Y1/Y2 = X1/X2 =',senilai)
+print('Maka Hasil Berbalik Nilai : Y1/Y2 = X2/X1 =',tinilai)
+print('==========================================================')
